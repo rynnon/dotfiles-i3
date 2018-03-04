81217df3c14dc0386a5b7a58c1b528b1e4fd9501
rynnon <r.steinbolt@gmail.com>
2018-03-04 14:12:25 +0100

    misc

--- .config/i3/increase-volume.sh
+++ .config/i3/increase-volume.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sink=`pactl list short sinks | grep RUNNING | cut -f1`                                                                                                                    
-notify-send "Volume +5%" -t 400
+#notify-send "Volume +5%" -t 400
 /usr/bin/pactl set-sink-volume $sink '+5%'
--- .config/i3/mute-volume.sh
+++ .config/i3/mute-volume.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sink=`pactl list short sinks | grep RUNNING | cut -f1`                                                                                                                    
-notify-send "mute volume toggle" -t 400
+#notify-send "mute volume toggle" -t 400
 /usr/bin/pactl set-sink-mute $sink toggle
